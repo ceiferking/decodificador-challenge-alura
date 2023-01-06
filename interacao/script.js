fcb35c7b3a5ace5c3b7ed3bcae1b43bf23d54a7c
Cristian Weber <47835197+ceiferking@users.noreply.github.com>
2023-01-05 21:08:04 -0300

    Update script.js

--- interacao/script.js
+++ interacao/script.js
@@ -6,21 +6,21 @@ function criptografar(){
 
   var texto = textInput.value;
 
-  var resultCripto = texto.replace(/e/g, "bd").replace(/i/g, "ad").replace(/a/g, "ha").replace(/o/g, "eu").replace(/u/g, "gig");
+  var resultCripto = texto.replace(/i/g, "ad").replace(/a/g, "ha").replace(/o/g, "eu").replace(/u/g, "gig").replace(/c/g, "ws").replace(/d/g, "s").replace(/e/g, "a").replace(/f/g, "gs").replace(/g/g, "kb").replace(/h/g, "ss").replace(/j/g, "lj").replace(/k/g, "qq").replace(/l/g, "qt").replace(/m/g, "cc").replace(/n/g, "xx").replace(/p/g, "hg");
 
   
   document.getElementById('output').innerHTML = '<textarea readonly id="input-text">' + resultCripto + 
-  '</textarea>' + '<button class="btn-copiar" id="copiar" onclick="copiar()">@Copiar@</button>'
+  '</textarea>' + '<button class="btn-copiar" id="copiar" onclick="copiar()">Copiar!</button>'
 }
 
 function descriptografar(){
 
     var texto = textInput.value;
 
-    var resultDescripto = texto.replace(/bd/g, "e").replace(/ad/g, "i").replace(/ha/g, "a").replace(/eu/g, "o").replace(/gig/g, "u");
+    var resultDescripto = texto.replace(/ad/g, "i").replace(/ha/g, "a").replace(/eu/g, "o").replace(/gig/g, "u").replace(/ws/g, "c").replace(/s/g, "d").replace(/a/g, "e").replace(/gs/g, "f").replace(/kb/g, "g").replace(/ss/g, "h").replace(/lj/g, "j").replace(/qq/g, "k").replace(/qt/g, "l").replace(/cc/g, "m").replace(/xx/g, "n").replace(/hg/g, "p");
   
     document.getElementById('output').innerHTML = '<textarea readonly id="input-text">' + resultDescripto + 
-    '</textarea>' + '<button class="btn-copiar" id="copiar" onclick="copiar()">@Copiar@</button>'
+    '</textarea>' + '<button class="btn-copiar" id="copiar" onclick="copiar()">Copiar!</button>'
 }
 
 function copiar() {
